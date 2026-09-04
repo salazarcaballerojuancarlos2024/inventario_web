7d9e1cde2f28114a7b1b361b4342c74c904b4cfa
Administrador <Administrador@pclenovocasa>
2026-09-04 22:30:38 +0200

    ultimo commit 04092026

--- invenario_web/src/main/java/com/inventario_web/InvenarioWebApplication.java
+++ invenario_web/src/main/java/com/inventario_web/InvenarioWebApplication.java
@@ -2,12 +2,19 @@ package com.inventario_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class InvenarioWebApplication {
+public class InvenarioWebApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InvenarioWebApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // Esto le dice a Tomcat que use esta clase como configuración
+        return application.sources(InvenarioWebApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(InvenarioWebApplication.class, args);
+    }
 }
